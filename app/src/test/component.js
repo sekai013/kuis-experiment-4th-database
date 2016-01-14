befc1739fc8b46bca7d25e7bc62daf683a1663b4
sekai013 <sekai013@gmail.com>
2016-01-14 20:40:03 +0900

    add threadview test

--- app/src/test/component.js
+++ app/src/test/component.js
@@ -11,6 +11,10 @@ const ThreadListItem = require('../client/js/components/ThreadListItem')
 const ThreadList = require('../client/js/components/ThreadList')
 const ThreadForm = require('../client/js/components/ThreadForm')
 const ThreadBox = require('../client/js/components/ThreadBox')
+const Question = require('../client/js/components/Question')
+const QuestionList = require('../client/js/components/QuestionList')
+const QuestionForm = require('../client/js/components/QuestionForm')
+const ThreadView = require('../client/js/components/ThreadView')
 
 describe('ThreadListItem', () => {
   let renderer = null
@@ -29,7 +33,7 @@ describe('ThreadListItem', () => {
     )
     const rendered = renderer.getRenderOutput()
     const expected = (
-      <tr className="question">
+      <tr className="threadListItem">
         <td><a href="/#threads/1">Test Thread 01</a></td>
         <td><a href="/#users/Foo">Foo</a></td>
         <td>2016/01/08 10:09:30</td>
@@ -58,7 +62,7 @@ describe('ThreadListItem', () => {
     )
     const rendered = renderer.getRenderOutput()
     const expected = (
-      <tr className="question">
+      <tr className="threadListItem">
         <td><a href="/#threads/1">Test Thread 01</a></td>
         <td><a href="/#users/Foo">Foo</a></td>
         <td>2016/01/08 10:09:30</td>
@@ -160,6 +164,36 @@ describe('ThreadList', () => {
     )
     expect(rendered).toEqualJSX(expected)
   })
+
+  it('onClickEdit', () => {
+    const spy = sinon.spy()
+    const rendered = TestUtils.renderIntoDocument(
+      <ThreadList size={2}
+                  items={items}
+                  page={1}
+                  isInDashboard={true}
+                  onClickEdit={spy} />
+    )
+    const btn = TestUtils.scryRenderedDOMComponentsWithTag(rendered, 'button')
+    expect(spy.called).toEqual(false)
+    TestUtils.Simulate.click(btn[0])
+    expect(spy.called).toEqual(true)
+  })
+
+  it('onClickDelete', () => {
+    const spy = sinon.spy()
+    const rendered = TestUtils.renderIntoDocument(
+      <ThreadList size={2}
+                  items={items}
+                  page={1}
+                  isInDashboard={true}
+                  onClickDelete={spy} />
+    )
+    const btn = TestUtils.scryRenderedDOMComponentsWithTag(rendered, 'button')
+    expect(spy.called).toEqual(false)
+    TestUtils.Simulate.click(btn[1])
+    expect(spy.called).toEqual(true)
+  })
 })
 
 describe('ThreadForm', () => {
@@ -169,20 +203,52 @@ describe('ThreadForm', () => {
     renderer = TestUtils.createRenderer()
   })
 
-  it('render', () => {
-    renderer.render(<ThreadForm onSubmitForm={() => {}} />)
+  it('render isEditMode=true', () => {
+    renderer.render(<ThreadForm isEditMode={true} onSubmitForm={() => {}} />)
     const rendered = renderer.getRenderOutput()
     const expected = (
-      <form className="threadForm" onSubmit={() => {}}>
-        <div className="form-group">
-          <label>Title</label>
-          <input type="text"
-                 className="form-control"
-                 value=""
-                 onChange={() => {}} />
+      <div className="panel panel-default">
+        <div className="panel-heading">
+          <h3 className="panel-title">Edit Thread</h3>
         </div>
-        <button type="submit" className="btn btn-default">Create</button>
-      </form>
+        <div className="panel-body">
+          <form className="threadForm form-inline" onSubmit={() => {}}>
+            <div className="form-group">
+              <label>Title</label>
+              <input type="text"
+                     className="form-control"
+                     value=""
+                     onChange={() => {}} />
+            </div>
+            <button type="submit" className="btn btn-default">Done</button>
+          </form>
+        </div>
+      </div>
+    )
+    expect(rendered).toEqualJSX(expected)
+  })
+
+  it('render isEditMode=false', () => {
+    renderer.render(<ThreadForm isEditMode={false} onSubmitForm={() => {}} />)
+    const rendered = renderer.getRenderOutput()
+    const expected = (
+      <div className="panel panel-default">
+        <div className="panel-heading">
+          <h3 className="panel-title">Create Thread</h3>
+        </div>
+        <div className="panel-body">
+          <form className="threadForm form-inline" onSubmit={() => {}}>
+            <div className="form-group">
+              <label>Title</label>
+              <input type="text"
+                     className="form-control"
+                     value=""
+                     onChange={() => {}} />
+            </div>
+            <button type="submit" className="btn btn-default">Create</button>
+          </form>
+        </div>
+      </div>
     )
     expect(rendered).toEqualJSX(expected)
   })
@@ -244,10 +310,402 @@ describe('ThreadBox', () => {
               </ul>
             </nav>
           </div>
-       
           <ThreadList size={10} page={1} items={[]} isInDashboard={false} />
         </div>
         <ThreadForm onSubmitForm={() => {}} />
+      </div>
+    )
+    expect(rendered).toEqualJSX(expected)
+  })
+})
+
+describe('Question', () => {
+  let renderer = null
+
+  beforeEach(() => {
+    renderer = TestUtils.createRenderer()
+  })
+
+  it('render isRequest=true', () => {
+    renderer.render(
+      <Question answer="Foo"
+                isRequest={true}
+                text={"This is a sample question.\nSecond Line."} />
+    )
+    const rendered = renderer.getRenderOutput()
+    const expected = (
+      <div className="question panel panel-default">
+        <div className="panel-heading">
+          <h2 className="panel-title">Requested Question</h2>
+        </div>
+        <div className="panel-body">
+          <p>This is a sample question.</p>
+          <p>Second Line.</p>
+          <div className="row">
+            <div className="col-sm-6">
+              <input type="text"
+                     className="form-control"
+                     onChange={() => {}}
+                     onBlur={() => {}} />
+            </div>
+            <div className="col-sm-3">
+              <button className="btn btn-default" disabled="disabled">-</button>
+            </div>
+            <div className="col-sm-3">
+              <button className="btn btn-default" onClick={() => {}}>
+                Show Answer
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+    expect(rendered).toEqualJSX(expected)
+  })
+
+  it('render isRequest=false', () => {
+    renderer.render(
+      <Question index={1}
+                answer="Foo"
+                isRequest={false}
+                text={"This is a sample question.\nSecond Line."} />
+    )
+    const rendered = renderer.getRenderOutput()
+    const expected = (
+      <div className="question panel panel-default">
+        <div className="panel-heading">
+          <h2 className="panel-title">Q1</h2>
+        </div>
+        <div className="panel-body">
+          <p>This is a sample question.</p>
+          <p>Second Line.</p>
+          <div className="row">
+            <div className="col-sm-6">
+              <input type="text"
+                     className="form-control"
+                     onChange={() => {}}
+                     onBlur={() => {}} />
+            </div>
+            <div className="col-sm-3">
+              <button className="btn btn-default" disabled="disabled">-</button>
+            </div>
+            <div className="col-sm-3">
+              <button className="btn btn-default" onClick={() => {}}>
+                Show Answer
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+    expect(rendered).toEqualJSX(expected)
+  })
+
+  it('render isEditActivated=true', () => {
+    renderer.render(
+      <Question index={1}
+                answer="Foo"
+                isRequest={false}
+                isEditActivated={true}
+                questionId={123}
+                text={"This is a sample question.\nSecond Line."} />
+    )
+    const rendered = renderer.getRenderOutput()
+    const expected = (
+      <div className="question panel panel-default">
+        <div className="panel-heading">
+          <h2 className="panel-title">Q1</h2>
+        </div>
+        <div className="panel-body">
+          <p>This is a sample question.</p>
+          <p>Second Line.</p>
+          <div className="row">
+            <div className="col-sm-6">
+              <input type="text"
+                     className="form-control"
+                     onChange={() => {}}
+                     onBlur={() => {}} />
+            </div>
+            <div className="col-sm-3">
+              <button className="btn btn-default" disabled="disabled">-</button>
+            </div>
+            <div className="col-sm-3">
+              <button className="btn btn-default" onClick={() => {}}>
+                Show Answer
+              </button>
+              <a href="/#questions/123/edit" className="btn btn-default">
+                Edit Question
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+    expect(rendered).toEqualJSX(expected)
+  })
+
+  it('onChangeAnswer', () => {
+    const rendered = TestUtils.renderIntoDocument(
+      <Question index={1}
+                answer="Foo"
+                isRequest={false}
+                text="This is a sample question." />
+    )
+    const input = TestUtils.scryRenderedDOMComponentsWithTag(rendered, 'input')
+
+    TestUtils.Simulate.change(input[0], { target: { value: 'Foo' } })
+    expect(rendered.state.userAnswer).toEqual('Foo')
+  })
+
+  it('onBlurAnswer', () => {
+    const rendered = TestUtils.renderIntoDocument(
+      <Question index={1}
+                answer="Foo"
+                isRequest={false}
+                text="This is a sample question." />
+    )
+    const input = TestUtils.scryRenderedDOMComponentsWithTag(rendered, 'input')
+
+    TestUtils.Simulate.change(input[0], { target: { value: 'Foo' } })
+    TestUtils.Simulate.blur(input[0])
+    expect(rendered.state.symbol).toEqual('◯')
+    TestUtils.Simulate.change(input[0], { target: { value: 'Bar' } })
+    TestUtils.Simulate.blur(input[0])
+    expect(rendered.state.symbol).toEqual('✕')
+  })
+
+  it('onClickShowAnswer', () => {
+    const rendered = TestUtils.renderIntoDocument(
+      <Question index={1}
+                answer="Foo"
+                isRequest={false}
+                text="This is a sample question." />
+    )
+    const btn = TestUtils.scryRenderedDOMComponentsWithTag(rendered, 'button')
+
+    expect(rendered.state.isShownAnswer).toEqual(false)
+    TestUtils.Simulate.click(btn[1])
+    expect(rendered.state.isShownAnswer).toEqual(true)
+  })
+})
+
+describe('QuestionList', () => {
+  let renderer = null
+
+  beforeEach(() => {
+    renderer = TestUtils.createRenderer()
+  })
+
+  it('render', () => {
+    const questions = [
+      {
+        questionId: 12,
+        _i: 1,
+        text: 'This is the first question.',
+        answer: 'Foo'
+      },
+      {
+        questionId: 14,
+        _i: 2,
+        text: 'This is the second question.',
+        answer: 'Bar'
+      },
+      {
+        questionId: 22,
+        _i: 3,
+        text: 'This is the third question.',
+        answer: 'Baz'
+      }
+    ]
+
+    renderer.render(
+      <QuestionList questions={questions} />
+    )
+    const rendered = renderer.getRenderOutput()
+    const expected = (
+      <div className="questionList">
+        <Question questionId={12}
+                  index={1}
+                  answer="Foo"
+                  text="This is the first question." />
+        <Question questionId={14}
+                  index={2}
+                  answer="Bar"
+                  text="This is the second question." />
+        <Question questionId={22}
+                  index={3}
+                  answer="Baz"
+                  text="This is the third question." />
+      </div>
+    )
+    expect(rendered).toEqualJSX(expected)
+  })
+})
+
+describe('QuestionForm', () => {
+  let renderer = null
+
+  beforeEach(() => {
+    renderer = TestUtils.createRenderer()
+  })
+
+  it('render isEditMode=true', () => {
+    renderer.render(
+      <QuestionForm isEditMode={true}
+                    text="Editmode"
+                    answer="Foo"
+                    threadId={1}
+                    index={1}
+                    onSubmitForm={() => {}} />
+    )
+    const rendered = renderer.getRenderOutput()
+    const expected = (
+      <div className="panel panel-default">
+        <div className="panel-heading">
+          <h3 className="panel-title">Edit Question</h3>
+        </div>
+        <div className="panel-body">
+          <form className="questionForm" onSubmit={() => {}}>
+            <div className="form-group">
+              <label>Text</label>
+              <textarea className="form-control"
+                        value="Editmode"
+                        onChange={() => {}} />
+            </div>
+            <div className="form-group">
+              <label>Answer</label>
+              <input type="text"
+                     className="form-control"
+                     value="Foo"
+                     onChange={() => {}} />
+            </div>
+            <button type="submit" className="btn btn-default">Done</button>
+          </form>
+        </div>
+      </div>
+    )
+
+    expect(rendered).toEqualJSX(expected)
+  })
+
+  it('render isEditMode=false', () => {
+    renderer.render(
+      <QuestionForm isEditMode={false} onSubmitForm={() => {}} />
+    )
+    const rendered = renderer.getRenderOutput()
+    const expected = (
+      <div className="panel panel-default">
+        <div className="panel-heading">
+          <h3 className="panel-title">Create Question</h3>
+        </div>
+        <div className="panel-body">
+          <form className="questionForm" onSubmit={() => {}}>
+            <div className="form-group">
+              <label>Text</label>
+              <textarea className="form-control" value="" onChange={() => {}} />
+            </div>
+            <div className="form-group">
+              <label>Answer</label>
+              <input type="text"
+                     className="form-control"
+                     value=""
+                     onChange={() => {}} />
+            </div>
+            <button type="submit" className="btn btn-default">Create</button>
+          </form>
+        </div>
+      </div>
+    )
+
+    expect(rendered).toEqualJSX(expected)
+  })
+
+  it('onChangeText', () => {
+    const rendered = TestUtils.renderIntoDocument(
+      <QuestionForm isEditMode={false} />
+    )
+    const textarea = TestUtils.scryRenderedDOMComponentsWithTag(rendered, 'textarea')
+
+    TestUtils.Simulate.change(textarea[0], { target: { value: 'Foo' } })
+    expect(rendered.state.text).toEqual('Foo')
+  })
+
+  it('onChangeAnswer', () => {
+    const rendered = TestUtils.renderIntoDocument(
+      <QuestionForm isEditMode={false} />
+    )
+    const input = TestUtils.scryRenderedDOMComponentsWithTag(rendered, 'input')
+
+    TestUtils.Simulate.change(input[0], { target: { value: 'Foo' } })
+    expect(rendered.state.answer).toEqual('Foo')
+  })
+
+  it('onSubmitForm isEditMode=true', () => {
+    const spy = sinon.spy()
+    const rendered = TestUtils.renderIntoDocument(
+      <QuestionForm isEditMode={true} onSubmitForm={spy}/>
+    )
+    const form = TestUtils.scryRenderedDOMComponentsWithTag(rendered, 'form')
+    const textarea = TestUtils.scryRenderedDOMComponentsWithTag(rendered, 'textarea')
+    const input = TestUtils.scryRenderedDOMComponentsWithTag(rendered, 'input')
+
+    expect(spy.called).toEqual(false)
+    TestUtils.Simulate.submit(form[0])
+    expect(spy.called).toEqual(false)
+    TestUtils.Simulate.change(textarea[0], { target: { value: 'Foo' } })
+    TestUtils.Simulate.submit(form[0])
+    expect(spy.called).toEqual(false)
+    TestUtils.Simulate.change(input[0], { target: { value: 'Foo' } })
+    TestUtils.Simulate.submit(form[0])
+    expect(spy.called).toEqual(false)
+  })
+
+  it('onSubmitForm isEditMode=false', () => {
+    const spy = sinon.spy()
+    const rendered = TestUtils.renderIntoDocument(
+      <QuestionForm isEditMode={false} onSubmitForm={spy}/>
+    )
+    const form = TestUtils.scryRenderedDOMComponentsWithTag(rendered, 'form')
+    const textarea = TestUtils.scryRenderedDOMComponentsWithTag(rendered, 'textarea')
+    const input = TestUtils.scryRenderedDOMComponentsWithTag(rendered, 'input')
+
+    expect(spy.called).toEqual(false)
+    TestUtils.Simulate.submit(form[0])
+    expect(spy.called).toEqual(false)
+    TestUtils.Simulate.change(textarea[0], { target: { value: 'Foo' } })
+    TestUtils.Simulate.submit(form[0])
+    expect(spy.called).toEqual(false)
+    TestUtils.Simulate.change(input[0], { target: { value: 'Foo' } })
+    TestUtils.Simulate.submit(form[0])
+    expect(spy.called).toEqual(true)
+  })
+})
+
+describe('ThreadView', () => {
+  let renderer = null
+
+  beforeEach(() => {
+    renderer = TestUtils.createRenderer()
+  })
+
+  it('render mode="questions"', () => {
+    renderer.render(<ThreadView threadId={1} mode="questions" />)
+    const rendered = renderer.getRenderOutput()
+    const expected = (
+      <div className="threadView">
+        <ul className="nav nav-tabs" role="tablist">
+          <li role="presentation" className="active">
+            <a href="/#threads/1" role="tab" data-toggle="tab">
+              Questions (0)
+            </a>
+          </li>
+          <li role="presentation" className="">
+            <a href="#threads/1/requests" role="tab" data-toggle="tab">
+              Requests (0)
+            </a>
+          </li>
+        </ul>
+        <QuestionList questions={[]} />
       </div>
     )
     expect(rendered).toEqualJSX(expected)
