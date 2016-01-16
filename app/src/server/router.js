b0002ff25a37aa080a42baa0be331064477955f9
sekai013 <sekai013@gmail.com>
2016-01-16 10:26:51 +0900

    add question edit/delete api

--- app/src/server/router.js
+++ app/src/server/router.js
@@ -188,4 +188,148 @@ router.post('/api/threads/:id/questions/create', auth, getDB, function *() {
   }
 })
 
+router.post('/api/threads/:id/questions/:index/delete', auth, getDB, function *() {
+  try {
+    const creater = yield this.DB.get(
+      'SELECT userId from create_thread where threadId = ?',
+      [this.params.id]
+    )
+    const target = yield this.DB.get(
+      'SELECT * FROM thread_question NATURAL JOIN questions NATURAL JOIN (SELECT * FROM threads WHERE threadId = ?) WHERE _i = ?',
+      [this.params.id, this.params.index]
+    )
+    this.target = target
+
+    if (creater && creater.userId === passport.id && target) {
+      const stmt1 = yield this.DB.prepare(
+        'DELETE FROM questions WHERE questionId = ?'
+      )
+      yield stmt1.run(target.questionId)
+      stmt1.finalize()
+      const stmt2 = yield this.DB.prepare(
+        'DELETE FROM thread_question WHERE questionId = ?'
+      )
+      yield stmt2.run(target.questionId)
+      stmt2.finalize()
+      yield this.DB.run(
+        'UPDATE questions SET questionId = questionId - 1 WHERE questionId > ?',
+        [target.questionId]
+      )
+      yield this.DB.run(
+        'UPDATE questions SET _i = _i - 1 WHERE _i > ?',
+        [target._i]
+      )
+      yield this.DB.run(
+        'UPDATE thread_question SET questionId = questionId - 1 WHERE questionId > ?',
+        [target.questionId]
+      )
+      this.body = { status: 200 }
+    } else {
+      this.status = 404
+      this.body = { status: 404, message: 'Not Found' }
+    }
+  } catch (error) {
+    console.error(error.toString());
+    this.status = 500
+    this.body = { status: 500, message: 'Internal Server Error' }
+    if (this.target) {
+      const question = yield this.DB.get(
+        'SELECT * FROM questions WHERE questionId = ?',
+        [this.target.questionId]
+      )
+      if (! question) {
+        const stmt3 = yield this.DB.prepare(
+          'INSERT INTO questions VALUES(?, ?, ?, ?, ?)'
+        )
+        yield stmt3.run(this.target.questionId,
+                        this.target.text,
+                        this.target.answer,
+                        this.target._i,
+                        0)
+        stmt3.finalize()
+      }
+      const questionsInThread = yield this.DB.prepare(
+        'SELECT * FROM thread_question WHERE questionId = ?',
+        [this.target.questionId]
+      )
+      if (! questionsInThread) {
+        const stmt4 = yield this.DB.prepare(
+          'INSERT INTO thread_question VALUES(?, ?)'
+        )
+        yield stmt4.run(this.params.id, this.target.questionId)
+        stmt4.finalize()
+      }
+    }
+  }
+})
+
+router.get('/api/questions/:id', getDB, function *() {
+  try {
+    const question = yield this.DB.get(
+      'SELECT * FROM questions WHERE questionId = ?',
+      [this.params.id]
+    )
+    this.status = question ? 200 : 404
+    this.body = question
+              ? { status: 200, question: question }
+              : { status: 404, message: 'Not Found' }
+  } catch (error) {
+    this.status = 500
+    this.body = { status: 500, message: 'Internal Server Error' }
+  }
+})
+
+router.post('/api/questions/:id/edit', auth, getDB, function *() {
+  try {
+    const creater = yield this.DB.get(
+      'SELECT userId, threadId FROM create_thread NATURAL JOIN thread_question where questionId = ?',
+      [this.params.id]
+    )
+
+    if (!creater) {
+      this.status = 404
+      this.body = { status: 404, message: 'Not Found' }
+      return
+    }
+
+    if (creater.userId !== passport.id) {
+      this.status = 401
+      this.body = { status: 401, message: 'Authorization Required' }
+      return
+    }
+
+    this.target = yield this.DB.get(
+      'SELECT * FROM questions WHERE questionId = ?',
+      [this.params.id]
+    )
+
+    yield this.DB.run(
+      'UPDATE questions SET text = ? WHERE questionId = ?',
+      [this.request.body.text, this.params.id]
+    )
+    yield this.DB.run(
+      'UPDATE questions SET answer = ? WHERE questionId = ?',
+      [this.request.body.answer, this.params.id]
+    )
+
+    this.body = { status: 200, threadId: creater.threadId }
+      
+  } catch (error) {
+    console.error(error.toString());
+    this.status = 500
+    this.body = { status: 500, message: 'Internal Server Error' }
+
+    if (this.target) {
+      yield this.DB.run(
+        'UPDATE questions SET text = ? WHERE questionId = ?',
+        [this.question.text, this.params.id]
+      )
+      yield this.DB.run(
+        'UPDATE questions SET answer = ? WHERE questionId = ?',
+        [this.question.answer, this.params.id]
+      )
+    }
+  }
+})
+
 module.exports = router
